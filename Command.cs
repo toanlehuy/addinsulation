a439fa9cd1cae593fa1f2f8a8e2f7907a707682c
toanlehuy <68963702+toanlehuy@users.noreply.github.com>
2021-07-20 12:48:57 +0700

    Add files via upload

---
+++ Command.cs
@@ -0,0 +1,36 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+#endregion
+
+namespace InsulationTool
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+
+            InsulationWPF insulationWPF = new InsulationWPF(uidoc);
+            insulationWPF.Topmost = true;
+            insulationWPF.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ InsulationWPF.xaml.cs
@@ -0,0 +1,110 @@
+﻿#region NameSpace
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion
+
+namespace InsulationTool
+{
+    /// <summary>
+    /// Interaction logic for InsulationWPF.xaml
+    /// </summary>
+    public partial class InsulationWPF : Window
+    {
+        UIDocument uidoc;
+        Document doc;
+        IList<Element> syspi;
+        IList<Element> sysdt;
+
+
+        public InsulationWPF(UIDocument uidoc)
+        {
+            InitializeComponent();
+            this.uidoc = uidoc;
+            doc = uidoc.Document;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IList<Element> level = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Levels)
+                .WhereElementIsNotElementType()
+                .ToElements();
+
+            syspi = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_PipingSystem)
+                .WhereElementIsElementType()
+                .ToElements();
+
+            sysdt = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_DuctSystem)
+                .WhereElementIsElementType()
+                .ToElements();
+
+
+            comlv.SelectedIndex = 0;
+            comlv.ItemsSource = level;
+            comlv.DisplayMemberPath = "Name";
+
+            comsys.ItemsSource = sysdt;
+            comsys.DisplayMemberPath = "Name";
+
+        }
+
+        private void butCan_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void raMa_Click(object sender, RoutedEventArgs e)
+        {
+            visFil(false);
+        }
+
+        private void raAu_Click(object sender, RoutedEventArgs e)
+        {
+            visFil(true);
+        }
+
+        public void visFil(bool a)
+        {
+            if(a == true)
+            {
+                comlv.IsEnabled = true;
+                comsys.IsEnabled = true;
+            }
+            else
+            {
+                comlv.IsEnabled = false;
+                comsys.IsEnabled = false;
+            }
+        }
+
+        private void raDt_Click(object sender, RoutedEventArgs e)
+        {
+            comsys.ItemsSource = sysdt;
+            comsys.DisplayMemberPath = "Name";
+        }
+
+        private void raPi_Click(object sender, RoutedEventArgs e)
+        {
+            comsys.ItemsSource = sysdt;
+            comsys.DisplayMemberPath = "Name";
+        }
+    }
+}
